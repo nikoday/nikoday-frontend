42972368eee3807a1ac8bdc313d5d50f1058eab6
Leandro Kersting de Freitas <leandro@frekele.org>
2020-12-11 04:35:28 -0300

    config team tab.

--- pages/team/Team.vue
+++ pages/team/Team.vue
@@ -34,13 +34,38 @@ export default {
   <div class="board">
 
     <router-link :to="'/edit-team/' + id">
-      <b-button variant="info"
+      <b-button variant="primary"
                 style="margin: 10px"
                 v-b-tooltip.hover
                 title="Edit Team">
         <i class="fa fa-pencil" aria-hidden="true"></i> Edit Team
       </b-button>
     </router-link>
+
+    <router-link :to="'/edit-team-user/' + id">
+      <b-button variant="secondary"
+                style="margin: 10px"
+                v-b-tooltip.hover
+                title="Edit Team">
+        <i class="fa fa-pencil" aria-hidden="true"></i> Config Team Users
+      </b-button>
+    </router-link>
+
+    <b-tabs content-class>
+      <b-tab title="Niko Niko Calendar" active>
+        <p>{{ text }}</p>
+        <p class="mb-0">{{ content }}</p>
+      </b-tab>
+      <b-tab title="Team Users">
+        <p>{{ content }}</p>
+        <p class="mb-0">{{ text }}</p>
+      </b-tab>
+      <b-tab title="Graphics">
+        <p>{{ content }}</p>
+        <p class="mb-0">{{ text }}</p>
+      </b-tab>
+    </b-tabs>
+
   </div>
 </template>
 
